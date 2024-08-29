b7fb514f714c365df2b5888d76431b0579bebd84
unknown <bandeira.freitas2013@gmail.com>
2024-08-28 22:19:10 -0300

    Aula 07 - parte 2

--- 07_WorkingWithVerbs/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/matheus/PersonController.java
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/matheus/PersonController.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +34,24 @@ public class PersonController {
 	public Person findById(@PathVariable(value = "id") String id) throws Exception {
 		
 		return service.findById(id);
+	}
+	
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) throws Exception {
+		
+		return service.create(person);
+	}
+	
+	@PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) throws Exception {
+		
+		return service.update(person);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void delete(@PathVariable(value = "id") String id) throws Exception {
+		
+		service.delete(id);
 	}
 
 
