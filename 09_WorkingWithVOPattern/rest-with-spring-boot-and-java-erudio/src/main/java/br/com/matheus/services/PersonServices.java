e7c71065cfebb2d6e6dfd17f3aa978fa63c32315
unknown <bandeira.freitas2013@gmail.com>
2024-09-11 19:39:27 -0300

    Modulo 09

---
+++ 09_WorkingWithVOPattern/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/matheus/services/PersonServices.java
@@ -0,0 +1,62 @@
+package br.com.matheus.services;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.matheus.data.vo.v1.PersonVO;
+import br.com.matheus.exceptions.ResourceNotFoundException;
+import br.com.matheus.repositories.PersonRepository;
+
+@Service
+public class PersonServices {
+	
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	@Autowired
+	PersonRepository repository;
+	
+	public List<PersonVO> findAll() {
+		logger.info("Finding all persons!");
+		
+		return repository.findAll();		
+	}
+
+	public PersonVO findById(Long id) {
+		logger.info("Finding one person!");
+		
+		PersonVO person = new PersonVO();
+		
+		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found this ID"));
+	}
+	
+	public PersonVO create(PersonVO person) {
+		logger.info("Creating one person!");
+		
+		return repository.save(person);
+	}
+	
+	public PersonVO update(PersonVO person) {
+		logger.info("Updating one person!");
+		
+		var entity = repository.findById(person.getId()).orElseThrow(() -> new ResourceNotFoundException("No records found this ID"));
+		
+		entity.setFirsName(person.getFirsName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return repository.save(entity);
+	}
+	
+	public void delete(Long id) {
+		logger.info("Deleting one person!");
+		
+		var entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found this ID"));
+		
+		repository.delete(entity);
+		
+	}
+}
