322da5de035aded9d90ce5f59b164fc469afbaa4
unknown <bandeira.freitas2013@gmail.com>
2024-10-08 20:47:19 -0300

    Aula 14 - parte 06

--- 14_HATEOAS/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/matheus/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ 14_HATEOAS/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/matheus/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.matheus.exceptions.ExceptionResponse;
+import br.com.matheus.exceptions.RequiredObjectIsNullException;
 import br.com.matheus.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -27,5 +28,10 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception ex, WebRequest request){
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+	@ExceptionHandler(RequiredObjectIsNullException.class)
+	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request){
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
 }
