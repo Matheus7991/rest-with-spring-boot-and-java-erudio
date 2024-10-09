394c0197e500e2f0c8ea661b99aff439d6b38114
unknown <bandeira.freitas2013@gmail.com>
2024-10-09 19:44:19 -0300

    Aula 15 - parte 01

---
+++ 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate__Table__Person2.sql
@@ -0,0 +1,3 @@
+INSERT INTO `person` (`id`,`address`,`firs_name`,`gender`,`last_name`) VALUES 
+	(1,'Recife - Pernambuco - Brasil','Matheus','M','Freitas'),
+	(2,'Recife - Pernambuco - tesfgwer - Brasil','Lala','F','Nelson');
