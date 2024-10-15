04936657ef5806e3b9b777e14a57e8b770cd4278
unknown <bandeira.freitas2013@gmail.com>
2024-10-14 21:58:33 -0300

    Aula 15 - Desafio - Testes

--- 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/matheus/mapper/DozerMapper.java
+++ 15_WorkingWithSwaggerOpenAPI/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/matheus/mapper/DozerMapper.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.modelmapper.ModelMapper;
 
+import br.com.matheus.data.vo.v1.BookVO;
 import br.com.matheus.data.vo.v1.PersonVO;
+import br.com.matheus.model.Book;
 import br.com.matheus.model.Person;
 
 //import com.github.dozermapper.core.DozerBeanMapperBuilder;
@@ -19,6 +21,11 @@ public class DozerMapper {
 	static {
 		mapper.createTypeMap(Person.class, PersonVO.class).addMapping(Person::getId, PersonVO::setKey);
 		mapper.createTypeMap(PersonVO.class, Person.class).addMapping(PersonVO::getKey, Person::setId);
+	}
+	
+	static {
+		mapper.createTypeMap(Book.class, BookVO.class).addMapping(Book::getId, BookVO::setKey);
+		mapper.createTypeMap(BookVO.class, Book.class).addMapping(BookVO::getKey, Book::setId);
 	}
 	
 	
