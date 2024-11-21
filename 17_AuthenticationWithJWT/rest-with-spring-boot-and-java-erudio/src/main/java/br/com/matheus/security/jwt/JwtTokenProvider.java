afeb4ae3af40c8e69bc339e1909a86605bd52a78
unknown <bandeira.freitas2013@gmail.com>
2024-11-21 20:18:30 -0300

    Aula 17 - P7

--- 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/matheus/security/jwt/JwtTokenProvider.java
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/matheus/security/jwt/JwtTokenProvider.java
@@ -44,12 +44,23 @@ public class JwtTokenProvider {
 			algorithm = Algorithm.HMAC256(secretKey.getBytes());	
 	}
 	
-	public TokenVO createAccesToken(String username, List<String> roles) {
+	public TokenVO createAccessToken(String username, List<String> roles) {
 		Date now = new Date();
 		Date validity = new Date(now.getTime() + validityInMilliseconds);
 		var accessToken = getAccessToken(username, roles, now, validity);
 		var refreshToken = getRefreshToken(username, roles, now);
 		return new TokenVO(username, true, now, validity, accessToken, refreshToken);
+	}
+	
+	public TokenVO refreshToken(String refreshToken) {
+		if (refreshToken.contains("Bearer ")) refreshToken =
+				refreshToken.substring("Bearer ".length());
+		
+		JWTVerifier verifier = JWT.require(algorithm).build();
+		DecodedJWT decodedJWT = verifier.verify(refreshToken);
+		String username = decodedJWT.getSubject();
+		List<String> roles = decodedJWT.getClaim("roles").asList(String.class);
+		return createAccessToken(username, roles);
 	}
 
 	private String getAccessToken(String username, List<String> roles, Date now, Date validity) {
