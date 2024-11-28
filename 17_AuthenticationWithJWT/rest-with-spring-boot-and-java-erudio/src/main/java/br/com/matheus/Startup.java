bb736cea24593a4fa316cf8260de50d1e814e738
unknown <bandeira.freitas2013@gmail.com>
2024-11-28 20:04:35 -0300

    Aula 17 - Refatorando

--- 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/matheus/Startup.java
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/matheus/Startup.java
@@ -2,6 +2,8 @@ package br.com.matheus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
 
 @SpringBootApplication
 public class Startup {
@@ -9,11 +11,13 @@ public class Startup {
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
 		
+		Pbkdf2PasswordEncoder  pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+		
         /**
         Map<String, PasswordEncoder> encoders = new HashMap<>();
-        encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
+        encoders.put("pbkdf2", pbkdf2Encoder);
         DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
-        passwordEncoder.setDefaultPasswordEncoderForMatches(new Pbkdf2PasswordEncoder());
+        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
         
         String result = passwordEncoder.encode("admin234");
         System.out.println("My hash " + result);*/
